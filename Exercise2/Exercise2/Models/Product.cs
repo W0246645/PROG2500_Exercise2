220e981f87dffe3cc470fd3a7ef33d61fcd97a87
Matthew Covey <90411582+W0246645@users.noreply.github.com>
2023-02-28 16:32:55 -0400

    Formatted a little bit.

---
+++ Exercise2/Exercise2/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2.Models
+{
+    public partial class Product
+    {
+        public string FormattedPrice
+        {
+            get
+            {
+                return string.Format("${0:N2}", UnitPrice);
+            }
+        }
+    }
+}
